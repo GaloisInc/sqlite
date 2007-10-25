6aa97246d90766cd78b8e8a4791485db1ef28a38
Sigbjorn Finne <sigbjorn.finne@gmail.com>
2007-10-25 06:52:27 +0200

    Test table creation

--- tests/Main.hs
+++ tests/Main.hs
@@ -2,9 +2,26 @@ module Main where
 
 import SQLite
 
+newTable :: TableName -> Table SQLType
+newTable tName = 
+  Table { tabName    = tName
+        , tabColumns = 
+	    [ Column { colName    = "id"
+	             , colType    = SQLInt NORMAL False False
+		     , colClauses = [PrimaryKey,AutoIncrement]
+		     }
+	    , Column { colName    = "name"
+	             , colType    = SQLVarChar 200
+		     , colClauses = [IsNullable False]
+		     }
+            ]
+        , tabConstraints = []
+	}
+
 main :: IO ()
 main = do
   h <- openConnection "test.db"
+  defineTable h (newTable "names")
   insertRow h "Poststed" [("Postnr", "4276"),("Poststed", "Vea")]
   ls <- execStatement h "SELECT * FROM Poststed;"
   print ls
