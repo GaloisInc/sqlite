4c31ce03e6b92ccd72cad40d337a3524e5c0290d
Sigbjorn Finne <sof@galois.com>
2008-01-11 15:11:45 -0800

    added nullable field to test table; robustify define/insert actions

--- tests/Main.hs
+++ tests/Main.hs
@@ -6,24 +6,37 @@ newTable :: TableName -> Table SQLType
 newTable tName = 
   Table { tabName    = tName
         , tabColumns = 
-            [ Column { colName    = "id"
-                     , colType    = SQLInt NORMAL False False
-                     , colClauses = [PrimaryKey,AutoIncrement]
-                     }
-            , Column { colName    = "name"
-                     , colType    = SQLVarChar 200
-                     , colClauses = [IsNullable False]
-                     }
+	    [ Column { colName    = "id"
+	             , colType    = SQLInt NORMAL False False
+		     , colClauses = [PrimaryKey,AutoIncrement]
+		     }
+	    , Column { colName    = "name"
+	             , colType    = SQLVarChar 200
+		     , colClauses = [IsNullable False]
+		     }
+	    , Column { colName    = "age"
+	             , colType    = SQLVarChar 200
+		     , colClauses = [IsNullable True]
+		     }
             ]
         , tabConstraints = []
-        }
+	}
 
 main :: IO ()
 main = do
-  h <- openConnection "test.db"
-  defineTable h (newTable "names")
-  insertRow h "Poststed" [("Postnr", "4276"),("Poststed", "Vea")]
-  ls <- execStatement h "SELECT * FROM Poststed;"
+  let dbFile = "test.db"
+  let ign act = 
+        catch (act >> return ()) 
+              (\ err -> putStrLn ("Error (but ignoring) -- " ++ show err) >> return ())
+  h <- openConnection dbFile
+  ign $ defineTable h (newTable "names")
+  ign $ insertRow h "Poststed" [("Postnr", "4278"),("Poststed", "Veakrossen")]
+  ls <- execStatement h "SELECT * FROM Poststed WHERE PostNr=4276;"
+  print ls
+  ign $ insertRow h "names" [("id", "1"),("name", "foo"), ("age", "30")]
+  ls <- execStatement h "SELECT * FROM names;"
+  print ls
+  ls <- execStatement h "SELECT * FROM names where id=1;"
   print ls
   closeConnection h
 
