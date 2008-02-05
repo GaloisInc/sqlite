02187910939b6a2ceda7580cbc4d3919f232e034
Iavor S. Diatchki <diatchki@galois.com>
2008-02-05 13:35:52 -0800

    Perform UTF8 encoding and decoding in execParamStmt

--- Database/SQLite.hs
+++ Database/SQLite.hs
@@ -55,6 +55,7 @@ import Data.IORef
 import Data.List
 import Data.Char ( isDigit )
 import Control.Monad (when, (<=<))
+import qualified Codec.Binary.UTF8.String as UTF8
 
 ------------------------------------------------------------------------
 
@@ -147,15 +148,15 @@ foreign import ccall "stdlib.h &free"
   p_free :: FunPtr (Ptr a -> IO ())
 
 
--- XXX: UTF8 encode!
--- XXX: will the destructor for TEXT be called if an error occurs?
+-- | Sets the value of a parameter in a statement.
+-- Perofrms UTF8 encoding.
 bindValue :: SQLiteStmt -> String -> Value -> IO Status
 bindValue stmt key value =
-  withCString key $ \ckey ->
+  withCString (UTF8.encodeString key) $ \ckey ->
   sqlite3_bind_parameter_index stmt ckey >>= \ix ->
   case value of
     Text txt ->
-      do (cptr,len) <- newCStringLen txt
+      do (cptr,len) <- newCStringLen (UTF8.encodeString txt)
          res <- sqlite3_bind_text stmt ix cptr (fromIntegral len) p_free
          -- XXX: should we be doing this?
          when (res /= sQLITE_OK) (free cptr)
@@ -166,17 +167,20 @@ bindValue stmt key value =
     Double x -> sqlite3_bind_double stmt ix x
 
 
+-- | Called when we know that an error has occured.
 to_error :: SQLite -> IO (Either String a)
 to_error db = Left `fmap` (peekCString =<< sqlite3_errmsg db)
 
--- XXX: We should be UTF8 encoding the queries.
+
+
 execParamStatement :: SQLite -> String -> [(String,Value)]
                    -> IO (Either String [Row])
 execParamStatement db query params =
   alloca $ \stmt_ptr ->
   alloca $ \pzTail ->
-  withCString query $ \zSql ->
-    do let nByte = fromIntegral (length query)
+  let encoded = UTF8.encodeString query in
+  withCString encoded $ \zSql ->
+    do let nByte = fromIntegral (length encoded)
        res <- sqlite3_prepare db zSql nByte stmt_ptr pzTail
        if res /= sQLITE_OK then to_error db else
          do stmt <- peek stmt_ptr
@@ -194,19 +198,19 @@ execParamStatement db query params =
        let cols = [0..col_num-1]
        names <- mapM peekCString =<<
                         mapM (sqlite3_column_name stmt) cols
-       -- XXX: rememeber to decode the "names"
-       get_rows stmt cols names []
+       let decoded_names = map UTF8.decodeString names
+       get_rows stmt cols decoded_names []
 
   get_rows stmt cols col_names rows =
     do res <- sqlite3_step stmt
        case () of
          _ | res == sQLITE_ROW ->
            do txts <- mapM (peekCString <=< sqlite3_column_text stmt) cols
-              let row = zip col_names txts
+              let row = zip col_names (map UTF8.decodeString txts)
               get_rows stmt cols col_names (row:rows)
            | res == sQLITE_DONE -> do sqlite3_finalize stmt
                                       return (Right (reverse rows))
-         _ -> sqlite3_finalize stmt >> to_error db
+           | otherwise -> sqlite3_finalize stmt >> to_error db
 
 
 
