094d9b9b4936580bb023963e42c3195bcb2a5519
Eric Mertens <emertens@galois.com>
2008-03-17 14:51:06 -0700

    convert registration functions to use Bool and Status types

--- vfs/Database/SQLite/VFS.hs
+++ vfs/Database/SQLite/VFS.hs
@@ -2,10 +2,11 @@
 module Database.SQLite.VFS where
 
 import Foreign.C
+import Database.SQLite.Types
 
 foreign import ccall "little.h register_little_vfs"
-  register_little_vfs :: CInt -> IO CInt
+  register_little_vfs :: Bool -> IO Status
 
 foreign import ccall "little.h register_little_ro_vfs"
-  register_little_ro_vfs :: CInt -> IO CInt
+  register_little_ro_vfs :: Bool -> IO Status
 
