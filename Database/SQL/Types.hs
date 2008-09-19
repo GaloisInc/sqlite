4c6ea9b5b96bf1a7f096716483e65b2957a8c330
Sigbjorn Finne <sof@galois.com>
2008-09-19 06:12:21 -0700

    micromods, no change in func.

--- Database/SQL/Types.hs
+++ Database/SQL/Types.hs
@@ -10,8 +10,9 @@
 --
 -- Basic embedding of SQL types in Haskell.
 --
--- Note: the quary part of this modules was imported (with modifications)
+-- Note: the query part of this modules was imported (with modifications)
 -- from the lowest layer of abstraction of HaskellDB.
+--
 module Database.SQL.Types
        ( TableName
        , ColumnName
@@ -53,22 +54,22 @@ import Data.List ( intersperse )
 import Text.PrettyPrint.HughesPJ
 
 type DatabaseName = String
-type TableName = String
-type ColumnName = String
-type OpName     = String
+type TableName    = String
+type ColumnName   = String
+type OpName       = String
 
 data Clause
- = IsNullable Bool
+ = IsNullable   Bool
  | DefaultValue String
- | PrimaryKey Bool    -- ^ Auto-increment?
- | ForeignKey TableName [ColumnName]
- | Clustered Bool
+ | PrimaryKey   Bool    -- ^ True => auto-increment.
+ | ForeignKey   TableName [ColumnName]
+ | Clustered    Bool
  | Unique
 
 data Constraint
   = TablePrimaryKey [ColumnName]
-  | TableUnique [ColumnName]
-  | TableCheck SQLExpr
+  | TableUnique     [ColumnName]
+  | TableCheck      SQLExpr
 
 data Table a
  = Table { tabName        :: String
@@ -97,8 +98,8 @@ data Column a
 -- too fancy..
 data SQLType
  = SQLBoolean
- | SQLChar    (Maybe Int)
- | SQLVarChar Int
+ | SQLChar     (Maybe Int)
+ | SQLVarChar  Int
  | SQLBlob     BlobType
  | SQLDateTime DateTimeType
  | SQLInt      IntType Bool{-unsigned?-} Bool{-zero fill-}
@@ -138,7 +139,7 @@ showType t =
 
     SQLDateTime dt ->
       case dt of
-         DATE -> "DATE"
+         DATE      -> "DATE"
          DATETIME  -> "DATETIME"
          TIMESTAMP -> "TIMESTAMP"
          TIME      -> "TIME"
@@ -163,12 +164,10 @@ showType t =
         case sequence [mbDig,mbScale] of 
            Nothing -> ""
            Just xs -> '(':concat (intersperse "," (map show xs)) ++ ")"
-    SQLEnum tgs ->  
-        "ENUM(" ++ toTags tgs ++ ")"
-    SQLSet tgs -> 
-        "SET(" ++ toTags tgs ++ ")"
+    SQLEnum tgs -> toTags "ENUM" tgs
+    SQLSet tgs  -> toTags "SET" tgs
   where
-    toTags xs = concat $ intersperse "," (map quote xs)
+    toTags l xs = l ++ '(':concat (intersperse "," (map quote xs)) ++ ")"
 
     quote nm = '\'':nm ++ "'"
 
