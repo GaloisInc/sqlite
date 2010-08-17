6f550f0fcccd5621156f04d8a5d24e4708bf8030
Adam Wick <awick@galois.com>
2010-08-17 14:00:41 -0700

    Add support for strings that start with numbers; bug report by Nikolas Mayr.

--- tests/Main.hs
+++ tests/Main.hs
@@ -32,19 +32,30 @@ main = do
               (\ err -> putStrLn ("Error (but ignoring) -- " ++ show err) >> return ())
   h <- openConnection dbFile
   ign $ defineTable h (newTable "names")
+  --
   ign $ insertRow h "Poststed" [("Postnr", "4278"),("Poststed", "Veakrossen")]
   ls <- execStatement h "SELECT * FROM Poststed WHERE PostNr=4276;"
   print (ls :: Either String [[Row Value]])
+  --
+  ign $ insertRow h "names" [("id", "2357"), ("name", "1,5m bis 3,0m"), ("age","35")]
+  -- This test a bug reported by Nikolas Mayr
+  ls <- execStatement h "SELECT * FROM names WHERE id=2357;"
+  print (ls :: Either String [[Row Value]])
+  --
   ign $ insertRow h "names" [("id", "1"),("name", "foo"), ("age", "30")]
   ls <- execStatement h "SELECT * FROM names;"
   print (ls :: Either String [[Row String]])
+  --
   ls <- execStatement h "SELECT * FROM names where id=1;"
   print (ls :: Either String [[Row String]])
+  --
   createFunction h "hi" (sum :: [Int] -> Int)
   ls <- execStatement h "SELECT hi(1,2,3,4,5) AS greeting"
   print (ls :: Either String [[Row String]])
+  --
   createFunction h "say" putStrLn
   ls <- execStatement h "SELECT say('Hi') AS greeting"
   print (ls :: Either String [[Row String]])
+  --
   closeConnection h
 
